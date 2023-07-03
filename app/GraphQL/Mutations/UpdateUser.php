a3fd891defa67c56b15eaba58c749bf9936085f6
anujpanchal <anujp@webmobtech.com>
2023-07-03 14:59:47 +0530

    initial commit

---
+++ app/GraphQL/Mutations/UpdateUser.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\GraphQL\Mutations;
+
+final class UpdateUser
+{
+    /**
+     * @param  null  $_
+     * @param  array{}  $args
+     */
+    public function __invoke($_, array $args)
+    {
+        dd("hi");
+        // TODO implement the resolver
+    }
+}
